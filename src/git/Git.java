6cf8f61be8d8960b1599b0d124532a12b8df2c61
Mancska <franyokrisz@gmail.com>
2023-09-20 12:51:43 +0200

    metodus

--- src/git/Git.java
+++ src/git/Git.java
@@ -2,12 +2,17 @@
 package git;
 public class Git {
     public static void main(String[] args) {
+        metodus();
         for (int i = 0; i < 10; i++) {
             
         }
         if(true){
             
         }
+    }
+
+    private static void metodus() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
     
 }
